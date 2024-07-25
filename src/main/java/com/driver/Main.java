26c833c4dff2a5c136e6697b131c299ab65d5ce8
siddardha777 <siddardhasiginam@gmail.com>
2024-07-25 11:14:41 +0530

    Inheritance

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
-  
+
+    static class A {
+        String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    static class B extends A {
+        @Override
+        String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
+
+        public static void main(String[] args) {
+            B objB = new B();
+            System.out.println(objB.meth()); // Output: Method is overridden in Extended class B
+        }
+
+
+
 }
